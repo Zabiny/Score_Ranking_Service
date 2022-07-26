19ba3ce563111b2fc32b06bed41f431f6564a9c5
Zhibin Yin <837332546@qq.com>
2022-07-26 19:36:33 +0800

    0726

---
+++ SkipList_lib/skiplist.h
@@ -0,0 +1,85 @@
+#include <iostream> 
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <mutex>
+#include <fstream>
+
+#define STORE_FILE "store/dumpFile"
+
+std::mutex mtx;     // mutex for critical section
+std::string delimiter = ":";
+
+//Class template to implement node
+template<typename K, typename V> 
+class Node {
+
+public:
+    
+    Node() {} 
+
+    Node(K k, V v, int); 
+
+    ~Node();
+
+    K get_key() const;
+
+    V get_value() const;
+
+    void set_value(V);
+    
+    // Linear array to hold pointers to next node of different level
+    Node<K, V> **forward;
+    //   [level1]
+    //  [level2]
+    // [level3]
+    //[level4]
+
+    int node_level;
+
+private:
+    K key;
+    V value;
+};
+
+// Class template for Skip list
+template <typename K, typename V> 
+class SkipList {
+
+public: 
+    SkipList(int);
+    ~SkipList();
+    int get_random_level();
+    Node<K, V>* create_node(K, V, int);
+    int insert_element(K, V);
+    void display_list();
+    bool search_element(K);
+    void delete_element(K);
+    void dump_file();
+    void load_file();
+    int size();
+
+private:
+    void get_key_value_from_string(const std::string& str, std::string* key, std::string* value);
+    bool is_valid_string(const std::string& str);
+
+private:    
+    // Maximum level of the skip list 
+    int _max_level;
+
+    // current level of skip list 
+    int _skip_list_level;
+
+    // pointer to header node 
+    Node<K, V> *_header;
+
+    // file operator
+    std::ofstream _file_writer;
+    std::ifstream _file_reader;
+
+    // skiplist current element count
+    int _element_count;
+};
+
+
+// vim: et tw=100 ts=4 sw=4 cc=120
